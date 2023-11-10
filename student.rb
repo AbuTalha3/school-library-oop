69f26076e12f96df2e0fb62aa5edd1d839db2601
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-11-11 00:17:54 +0500

    Add relation student to class room

--- student.rb
+++ student.rb
@@ -5,6 +5,8 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(id, age, name: 'Unknown', parent_permission: true, classroom: 'Unknown')
+    attr_reader : classroom
+    
     super(id, age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
