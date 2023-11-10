2c418910e286a4c10306e51dfad17c0b04052e7f
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-11-10 18:53:28 +0500

    Remove linter errors

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -4,6 +4,5 @@ class CapitalizeDecorator < BaseDecorator
   def correct_name
     original_name = @nameable.correct_name
     capitalized_name = original_name.capitalize
-    return capitalized_name
   end
 end
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -4,6 +4,5 @@ class TrimmerDecorator < BaseDecorator
   def correct_name
     original_name = @nameable.correct_name
     trimmed_name = original_name[0, 10] # Keep the first 10 characters
-    return trimmed_name
   end
 end
